3f4452b79c09e592395d1a0615e3c918040482c4
IS <russland@yandex.ru>
2017-03-20 06:18:28 +0300

    Chapter 9 ready.

--- app/src/main/java/ru/coffeeplanter/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/ru/coffeeplanter/criminalintent/SingleFragmentActivity.java
@@ -4,25 +4,25 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
-public class CrimeActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        Log.d("SingleFragmentActivity", "onCreate");
         setContentView(R.layout.activity_fragment);
-
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragmentContainer, fragment)
                     .commit();
         }
-
     }
 
 }
